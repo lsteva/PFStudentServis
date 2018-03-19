222df8aaaf426dc6d34cc2dea2fe944499e75ec4
Stevan Lukić <lsteva@hotmail.com>
2018-03-19 22:45:43 +0100

    Zavrsena login funkcionalnost (testirano prema bazi)

---
+++ PFStudentServis/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using PFStudentServis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PFStudentServis.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autorizacija(PFStudentServis.Models.Student studentModel)
+        {
+            using (PFServisEntities db = new PFServisEntities())
+            {
+                var brojIndeksaDetalji = db.Students.Where(x => x.BrojIndeksa == studentModel.BrojIndeksa && x.JMBG == studentModel.JMBG).FirstOrDefault();
+                if (brojIndeksaDetalji == null)
+                {
+                    studentModel.LoginErrorMessage = "Pogrešan broj indeksa ili JMBG.";
+                    return View("Index", studentModel);
+         }
+                else
+                { Session["userID"] = brojIndeksaDetalji.UserID;
+                    Session["brojIndeksa"] = brojIndeksaDetalji.Ime;
+                    Session["brojIndeksa1"] = brojIndeksaDetalji.Prezime;
+                }
+                return RedirectToAction("Index", "Home");
+            }
+
+ 
+        }
+        public ActionResult LogOut()
+        {
+            int brojIndeksa = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+       
+    }
+}
